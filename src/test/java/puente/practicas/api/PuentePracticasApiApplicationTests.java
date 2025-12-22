37a4f01e2d0337046744d1acc788eab5be01bcf1
jose alberto cruz martinez <albertocruz8133@proton.me>
2025-12-21 18:35:21 -0600

    init project

---
+++ src/test/java/puente/practicas/api/PuentePracticasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package puente.practicas.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuentePracticasApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
