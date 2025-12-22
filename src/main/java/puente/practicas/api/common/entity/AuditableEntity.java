0fbe6a6c250a550bec7c448c754f5d1c07d39a3d
jose alberto cruz martinez <albertocruz8133@proton.me>
2025-12-21 18:38:11 -0600

    feat: Agrega clase para propiedades de auditoria como 'created_at' y 'updated_at'

---
+++ src/main/java/puente/practicas/api/common/entity/AuditableEntity.java
@@ -0,0 +1,30 @@
+package puente.practicas.api.common.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+public class AuditableEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private Instant createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private Instant updatedAt;
+}
