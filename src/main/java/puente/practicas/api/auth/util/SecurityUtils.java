534ad6b289a2f2befedfe54bda62d4850459257a
jose alberto cruz martinez <albertocruz8133@proton.me>
2025-12-29 15:53:41 -0600

    feat: Agrega metodo de busqueda de ID de usuario a traves del email

--- src/main/java/puente/practicas/api/auth/util/SecurityUtils.java
+++ src/main/java/puente/practicas/api/auth/util/SecurityUtils.java
@@ -1,11 +1,18 @@
 package puente.practicas.api.auth.util;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+import puente.practicas.api.auth.service.interfaces.UserService;
+
+import java.util.UUID;
 
 @Component
+@RequiredArgsConstructor
 public class SecurityUtils {
+
+    private final UserService userService;
 
     public String getAuthenticatedUserEmail() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -13,5 +20,10 @@ public class SecurityUtils {
         if (authentication == null) throw new IllegalStateException("No authentication found in security context");
 
         return authentication.getName();
+    }
+
+    public UUID getAuthenticatedUserId() {
+        String email = getAuthenticatedUserEmail();
+        return userService.findIdByEmail(email);
     }
 }
