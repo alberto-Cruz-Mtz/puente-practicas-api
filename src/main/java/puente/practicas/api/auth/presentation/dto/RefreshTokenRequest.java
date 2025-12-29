ed2efa6f28079efd0806552ad31406753dc0f594
jose alberto cruz martinez <albertocruz8133@proton.me>
2025-12-28 22:20:19 -0600

    refactor: Renombra el dto RefreshToken a RefreshTokenRequest para mayor claridad de entrada de datos

--- src/main/java/puente/practicas/api/auth/presentation/dto/RefreshTokenRequest.java
+++ src/main/java/puente/practicas/api/auth/presentation/dto/RefreshTokenRequest.java
@@ -2,7 +2,7 @@ package puente.practicas.api.auth.presentation.dto;
 
 import jakarta.validation.constraints.NotEmpty;
 
-public record RefreshToken(
+public record RefreshTokenRequest(
         @NotEmpty(message = "Refresh refreshToken must not be empty") String refreshToken
 ) {
 }
