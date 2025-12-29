c9475b81110b95ae1ccece8cde7c2f96675126a1
jose alberto cruz martinez <albertocruz8133@proton.me>
2025-12-29 16:08:09 -0600

    feat: Agrega metodo personalizado en el repositorio para devolver unicamente el id del usuario

--- src/main/java/puente/practicas/api/auth/persistence/repository/UserRepository.java
+++ src/main/java/puente/practicas/api/auth/persistence/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package puente.practicas.api.auth.persistence.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import puente.practicas.api.auth.persistence.entity.UserEntity;
 
 import java.util.Optional;
@@ -9,6 +10,9 @@ import java.util.UUID;
 public interface UserRepository extends JpaRepository<UserEntity, UUID> {
 
     Optional<UserEntity> findByEmail(String email);
+
+    @Query("SELECT u.uid FROM UserEntity u WHERE u.email = :email")
+    Optional<UUID> findIdByEmail(String email);
 
     boolean existsByEmail(String email);
 }
