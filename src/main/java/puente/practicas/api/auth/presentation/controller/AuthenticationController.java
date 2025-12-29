3783321ff8a2a1ca25f0dd3e972422bc7b62e9ed
jose alberto cruz martinez <albertocruz8133@proton.me>
2025-12-28 22:32:13 -0600

    feat: Agrega anotaciones @Valid a las entradas del controlador

--- src/main/java/puente/practicas/api/auth/presentation/controller/AuthenticationController.java
+++ src/main/java/puente/practicas/api/auth/presentation/controller/AuthenticationController.java
@@ -1,21 +1,21 @@
 package puente.practicas.api.auth.presentation.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import puente.practicas.api.auth.presentation.dto.AuthenticationRequest;
 import puente.practicas.api.auth.presentation.dto.AuthenticationResponse;
-import puente.practicas.api.auth.presentation.dto.RefreshToken;
+import puente.practicas.api.auth.presentation.dto.RefreshTokenRequest;
 import puente.practicas.api.auth.presentation.dto.RefreshTokenResponse;
+import puente.practicas.api.auth.presentation.dto.RegisterRequest;
 import puente.practicas.api.auth.service.interfaces.AuthenticationService;
-
-import java.util.UUID;
+import puente.practicas.api.auth.util.SecurityUtils;
 
 @RestController
 @RequestMapping("/auth")
@@ -23,30 +23,30 @@ import java.util.UUID;
 public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
+    private final SecurityUtils securityUtils;
 
     @PostMapping("/signup")
-    public ResponseEntity<AuthenticationResponse> signup(@RequestBody AuthenticationRequest request) {
+    public ResponseEntity<AuthenticationResponse> signup(@RequestBody @Valid RegisterRequest request) {
         var response = authenticationService.signup(request);
         return ResponseEntity.ok(response);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<AuthenticationResponse> login(@RequestBody AuthenticationRequest request) {
+    public ResponseEntity<AuthenticationResponse> login(@RequestBody @Valid AuthenticationRequest request) {
         var response = authenticationService.login(request);
         return ResponseEntity.ok(response);
     }
 
     @PostMapping("/logout")
     public ResponseEntity<Void> logout() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String currentPrincipalName = authentication.getName();
+        String currentPrincipalName = securityUtils.getAuthenticatedUserEmail();
 
         authenticationService.logout(currentPrincipalName);
         return ResponseEntity.noContent().build();
     }
 
     @PostMapping("/refresh-token")
-    public ResponseEntity<RefreshTokenResponse> refreshToken(@RequestBody RefreshToken request) {
+    public ResponseEntity<RefreshTokenResponse> refreshToken(@RequestBody @Valid RefreshTokenRequest request) {
         var response = authenticationService.refreshToken(request);
         return ResponseEntity.ok(response);
     }
