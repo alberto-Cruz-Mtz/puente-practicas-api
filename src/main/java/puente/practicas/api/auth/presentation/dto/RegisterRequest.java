3354d6b4c8fc1c9d1d5408b5f1330a53e421262a
jose alberto cruz martinez <albertocruz8133@proton.me>
2025-12-28 22:21:37 -0600

    refactor: Delega el AuthenticationRequest unicamente para login de usuario y añade RegisterRequest para signup

--- src/main/java/puente/practicas/api/auth/presentation/dto/RegisterRequest.java
+++ src/main/java/puente/practicas/api/auth/presentation/dto/RegisterRequest.java
@@ -4,14 +4,19 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import puente.practicas.api.auth.persistence.model.AccountType;
 
-public record AuthenticationRequest(
+public record RegisterRequest(
         @Email(message = "Email should be valid format address")
         @NotBlank(message = "Email is required")
         String email,
 
         @NotBlank(message = "Password is required")
         @Size(min = 8, max = 20, message = "Password must be between {min} and {max} characters")
-        String password
+        String password,
+
+        @Pattern(regexp = "STUDENT|RECRUITER", message = "Account type must be one of the predefined values")
+        @NotBlank(message = "Account type is required")
+        AccountType accountType
 ) {
 }
